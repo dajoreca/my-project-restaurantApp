612709d7b0b3987975fb706e3a8f9c4a4d77ac93
Daniel Reollo <dreollo92@gmail.com>
2022-06-01 17:05:41 +0200

    RestaurantAppBase

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react'
 
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import NuevaOrden from './views/NuevaOrden'
+import Menu from './views/Menu';
+import DetallePlatillo from './views/DetallePlatillo'
+import FormularioPlatillo from './views/FormularioPlatillo'
+import ResumenPedido from './views/DetallePlatillo'
+import ProgresoPedido from './views/DetallePlatillo'
+
+
+const Stack = createStackNavigator()
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+    <NavigationContainer>
+      <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor:'#FFDA00'
+        },
+        headerTitleStyle:{
+          fontweight:'bold'
+        }
+      }}
+      >
+        <Stack.Screen 
+          name="Nueva Orden"
+          component={NuevaOrden}
+          options={{
+            title: "Nueva Orden"
+          }}
+        /> 
+
+        <Stack.Screen 
+          name="Menu"
+          component={Menu}
+          options={{
+            title: "Nuestro Menu"
+          }}
+        /> 
+
+        <Stack.Screen 
+          name="DetallePlatillo"
+          component={DetallePlatillo}
+          options={{
+            title: "Detalle Platillo"
+          }}
+        /> 
+
+        <Stack.Screen 
+          name="FormularioPlatillo"
+          component={FormularioPlatillo}
+          options={{
+            title: "Ordenar Platillo"
+          }}
+        /> 
+
+        <Stack.Screen 
+          name="ResumenPedido"
+          component={ResumenPedido}
+          options={{
+            title: "Resumen Pedido"
+          }}
+        /> 
+
+        <Stack.Screen 
+          name="ProgresoPedido"
+          component={ProgresoPedido}
+          options={{
+            title: "Progreso de Pedido"
+          }}
+        /> 
+
+      </Stack.Navigator>
+    </NavigationContainer>
+    </>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ views/ProgresoPedido.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from "react-native";
+
+const ProgresoPedido = () => {
+    return ( 
+        <Text></Text>
+     );
+}
+ 
+export default ProgresoPedido;
