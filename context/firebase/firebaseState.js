447709fbdba76c0e0df50726fac58607f8773595
Daniel Reollo <dreollo92@gmail.com>
2022-06-15 13:38:15 +0200

    Add_CategoriesDivisor

--- context/firebase/firebaseState.js
+++ context/firebase/firebaseState.js
@@ -1,59 +1,64 @@
 import React, { useReducer } from "react";
 
-import firebase from '../../firebase';
-import FirebaseReducer from "./firebaseReducer"
-import FirebaseContext from "./firebaseContext"
+import firebase from "../../firebase";
+import FirebaseReducer from "./firebaseReducer";
+import FirebaseContext from "./firebaseContext";
 import { OBTENER_PRODUCTOS_EXITO } from "../../types";
 
-const FirebaseState = props => {
+import _ from "lodash";
 
-    console.log(firebase);
+const FirebaseState = (props) => {
+  //console.log(firebase);
 
-//Crear state inicial
-    const initialState = {
-        menu: []
+  //Crear state inicial
+  const initialState = {
+    menu: [],
+  };
+
+  //useReducer con dispatch para ejecutar las funciones
+  const [state, dispatch] = useReducer(FirebaseReducer, initialState);
+
+  const ObtenerProductos = () => {
+    //consultar firebase
+
+    firebase.db
+      .collection("productos")
+      .where("existencia", "==", true) //traer solo los que esten en existencia
+      .onSnapshot(manejarSnapshot);
+
+    function manejarSnapshot(snapshot) {
+      let platillos = snapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+
+      //ordenar por categoria con lodash
+
+      platillos = _.sortBy(platillos, "categoria");
+
+      //console.log(platillos);
+
+      //tenemos resultados de la base de datos
+      dispatch({
+        type: OBTENER_PRODUCTOS_EXITO,
+        payload: platillos,
+      });
     }
+  };
 
-    //useReducer con dispatch para ejecutar las funciones
-    const [state, dispatch] = useReducer(FirebaseReducer, initialState);
+  return (
+    <FirebaseContext.Provider
+      value={{
+        menu: state.menu,
+        firebase,
+        ObtenerProductos,
+      }}
+    >
+      {props.children}
+    </FirebaseContext.Provider>
+  );
+};
 
-    const ObtenerProductos = () => {
-
-
-        //consultar firebase
-
-        firebase.db
-            .collection('productos')
-            .where('existencia', '==', true) //traer solo los que esten en existencia
-            .onSnapshot(manejarSnapshot);
-
-            function manejarSnapshot(snapshot) {
-                let platillos = snapshot.docs.map(doc => {
-                    return{
-                        id: doc.id,
-                        ...doc.data()
-                    }
-                });
-
-                //tenemos resultados de la base de datos
-                dispatch({
-                    type: OBTENER_PRODUCTOS_EXITO,
-                    payload: platillos
-                });
-            }
-    }
-
-    return (
-        <FirebaseContext.Provider
-            value={{
-                menu: state.menu,
-                firebase,
-                ObtenerProductos
-            }}
-        >
-            {props.children}
-        </FirebaseContext.Provider>
-    )
-}
-
-export default FirebaseState
+export default FirebaseState;
--- views/Menu.js
+++ views/Menu.js
@@ -1,21 +1,122 @@
-import React, { useContext, useEffect } from "react";
-import { Text } from "react-native";
+import React, { useContext, useEffect, Fragment } from "react";
+import { StyleSheet, View } from "react-native";
 import FirebaseContext from "../context/firebase/firebaseContext";
+import {
+  Container,
+  Divider,
+  List,
+  ListItem,
+  Thumbnail,
+  Text,
+  InputLeftAddon,
+  Body,
+  Box,
+  Actionsheet,
+  Content,
+  Image,
+} from "native-base";
+
+import globalStyles from "../styles/global";
 
 const Menu = () => {
+  //context de firebase
+  const { menu, ObtenerProductos } = useContext(FirebaseContext);
 
-    //context de firebase
-    const { menu, ObtenerProductos } = useContext(FirebaseContext)
+  useEffect(() => {
+    ObtenerProductos();
+  }, []);
 
-    useEffect(() => {
-        ObtenerProductos();
+  const mostrarHeading = (categoria, i) => {
+    if (i > 0) {
+      const categoriaAnterior = menu[i - 1].categoria;
+      if (categoriaAnterior !== categoria) {
+        return (
+          <Divider style={styles.separador}>
+            <Text style={styles.separadorTexto}> {categoria} </Text>
+          </Divider>
+        );
+      }
+    } else {
+      return (
+        <Divider style={styles.separador}>
+          <Text style={styles.separadorTexto}> {categoria} </Text>
+        </Divider>
+      );
+    }
+  };
 
-        console.log(menu)
-    }, []);
+  return (
+    <Container style={globalStyles.contenedor}>
+      <View style={styles.box}>
+        <List>
+          {menu.map((platillo, i) => {
+            const { imagen, nombre, descripcion, categoria, precio, id } =
+              platillo;
+            //console.log(imagen);
 
-    return ( 
-        <Text>Menu</Text>
-     );
-}
- 
+            return (
+              <View key={id}>
+                <View>{mostrarHeading(categoria, i)}</View>
+
+                <View style={styles.contenedor}>
+                  <Text marginBottom={2} style={styles.textoNombre}>
+                    {nombre}
+                  </Text>
+
+                  <View style={styles.imgText}>
+                    <Image
+                      size={60}
+                      //resizeMode={"contain"}
+                      borderRadius={100}
+                      source={{ uri: imagen }}
+                      alt="Alternate Text"
+                    />
+
+                    <Text style={styles.description} note numberOfLines={2}>
+                      {descripcion}
+                    </Text>
+                    <Text marginRight={2}> Precio: {precio}</Text>
+                  </View>
+                </View>
+              </View>
+            );
+          })}
+        </List>
+      </View>
+    </Container>
+  );
+};
+
+const styles = StyleSheet.create({
+  contenedor: {
+    flex: 1,
+    padding: 20,
+    borderWidth: 1,
+    borderStyle: "solid",
+  },
+  box: {
+    backgroundColor: "#FFF",
+    width: "-webkit-fill-available",
+  },
+  textoNombre: {
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
+  description: {
+    margin: "auto",
+    marginRight: 10,
+    marginLeft: 20,
+  },
+  imgText: {
+    flexDirection: "row",
+  },
+  separador: {
+    backgroundColor: "#000",
+  },
+  separadorTexto: {
+    color: "#FFDA00",
+    fontWeight: "bold",
+  },
+});
+
 export default Menu;
